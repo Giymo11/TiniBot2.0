7fb72a7e44f47654e0cd9c22366f95f179c55cfb
Giymo11 <gizmo.head@yahoo.de>
2017-07-01 21:03:38 +0200

    Rewrites the bot to actually use Akka Typed

--- bot/src/main/scala/science/wasabi/tini/bot/BotMain.scala
+++ bot/src/main/scala/science/wasabi/tini/bot/BotMain.scala
@@ -1,8 +1,9 @@
 package science.wasabi.tini.bot
 
 import science.wasabi.tini._
-import science.wasabi.tini.bot.discord.{Discord4JIngestion, DiscordMessage, JavacordIngestion, JdaIngestion}
+import science.wasabi.tini.bot.discord._
 import science.wasabi.tini.config.Config
+import science.wasabi.tini.config.Config.TiniConfig
 
 object BotMain extends App {
   println(Helper.greeting)
@@ -16,19 +17,25 @@ object BotMain extends App {
   import akka.typed.scaladsl.Actor
 
   case class MessageActorState(name: String, count: Int = 0)
-  def messageActor(state: MessageActorState): Behavior[DiscordMessage] = Actor.immutable {
-    (ctx, message) =>
+  def printActor(state: MessageActorState): Behavior[DiscordMessage] = Actor.immutable {
+    (_, message) =>
       println(s"[${state.count}] ${state.name} sends ${message.content}")
-      messageActor(state.copy(count = state.count + 1))
+      printActor(state.copy(count = state.count + 1))
   }
 
+  // old way:
+  /*-----
   val system1: ActorSystem[DiscordMessage] =
-    ActorSystem("messageActorJda", messageActor(MessageActorState("JDA")))
+    ActorSystem("messageActorJda", printActor(MessageActorState("JDA")))
   val system2: ActorSystem[DiscordMessage] =
-    ActorSystem("messageActorJavacord", messageActor(MessageActorState("Javacord")))
+    ActorSystem("messageActorJavacord", printActor(MessageActorState("Javacord")))
 
   val ingestion1 = new JdaIngestion((message) => system1 ! message)
   val ingestion2 = new JavacordIngestion((message) => system2 ! message)
   //val ingestion3 = new Discord4JIngestion((message) => println("Discord4J says: " + message.content))
+  -----*/
+
+  implicit val messageHandler = printActor(MessageActorState("lol"))
+  JdaIngestionActor.startup
 }
 
--- bot/src/main/scala/science/wasabi/tini/bot/discord/JdaIngestion.scala
+++ bot/src/main/scala/science/wasabi/tini/bot/discord/JdaIngestion.scala
@@ -1,18 +1,21 @@
 package science.wasabi.tini.bot.discord
 
 
+import akka.typed.scaladsl.Actor
 import net.dv8tion.jda.core._
 import net.dv8tion.jda.core.events.ReadyEvent
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
-
 import science.wasabi.tini.config.Config.TiniConfig
+import DiscordWrapperConverter._
 
-
+/**
+  * A way to add listeners as lambdas
+  * @param listener
+  * @param config
+  */
 class JdaIngestion(listener: DiscordMessage => Unit)(implicit config: TiniConfig) {
-
-  import DiscordWrapperConverter._
 
   val jda: JDA =
     new JDABuilder(AccountType.BOT)
@@ -27,4 +30,59 @@ class JdaIngestion(listener: DiscordMessage => Unit)(implicit config: TiniConfig
         }
       })
       .buildAsync()
+}
+
+
+object JdaIngestionActor {
+
+  import akka.typed._
+
+  trait Commands
+  case class Ready(jda: JDA) extends Commands
+  case class Shutdown() extends Commands
+
+  def supervisor(jda: JDA): Behavior[Commands] =
+    Actor.immutable{ (ctx, message) =>
+      message match {
+        case event: Shutdown =>
+          println("Shutting down")
+          Actor.stopped
+      }
+    }
+
+  def starting(implicit messageHandler: Behavior[DiscordMessage]): Behavior[Commands] =
+    Actor.immutable { (ctx, message) =>
+      message match {
+        case event: Ready =>
+          println("JDA is ready")
+
+          val handler = ctx.spawn(messageHandler, "handler")
+
+          event.jda.addEventListener(new ListenerAdapter {
+            override def onGuildMessageReceived(event: GuildMessageReceivedEvent): Unit = {
+              handler ! event.getMessage
+            }
+            override def onPrivateMessageReceived(event: PrivateMessageReceivedEvent): Unit = {
+              handler ! event.getMessage
+            }
+          })
+          supervisor(event.jda)
+      }
+    }
+
+  /**
+    * A way to spawn an actor system to listen for messages.
+    * @param messageHandler
+    * @param config
+    */
+  def startup(implicit messageHandler: Behavior[DiscordMessage], config: TiniConfig) = {
+    val system: ActorSystem[Commands] = ActorSystem("jdaActor", starting)
+
+    val jda = new JDABuilder(AccountType.BOT)
+      .setToken(config.discordKey)
+      .addEventListener(new ListenerAdapter {
+        override def onReady(event: ReadyEvent): Unit = system ! Ready(event.getJDA)
+      })
+      .buildAsync()
+  }
 }
