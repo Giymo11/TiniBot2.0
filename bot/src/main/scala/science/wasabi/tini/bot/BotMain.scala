84ac380e0e42fc6e657cc407af8f613e39a92a09
Giymo11 <gizmo.head@yahoo.de>
2017-07-18 20:56:10 +0200

    works with kafka!

--- bot/src/main/scala/science/wasabi/tini/bot/BotMain.scala
+++ bot/src/main/scala/science/wasabi/tini/bot/BotMain.scala
@@ -2,14 +2,11 @@ package science.wasabi.tini.bot
 
 
 import akka.Done
-import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.ProducerRecord
 import science.wasabi.tini._
 import science.wasabi.tini.bot.discord.ingestion.AkkaCordIngestion
+import science.wasabi.tini.bot.kafka.KafkaStreams
 import science.wasabi.tini.config.Config
 
 import scala.concurrent.Future
@@ -25,38 +22,24 @@ object BotMain extends App {
   implicit val system = akka.actor.ActorSystem("kafka")
   implicit val materializer = ActorMaterializer()
 
-  import org.apache.kafka.common.serialization._
+  val streams = new KafkaStreams
 
-  val kafkaServer = s"${config.kafka.server}:${config.kafka.port}"
-
-  // kafka producer
-  val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
-    .withBootstrapServers(kafkaServer)
-
-  val doneProducer = ingestion.source
-    .map(_.toString)
-    .map(elem => {
-      println("in: " + elem)
-      new ProducerRecord[Array[Byte], String](config.kafka.topic, elem)
-    })
-    .runWith(Producer.plainSink(producerSettings))
+  // pipe to kafka
+  val discordMessageStream = ingestion.source
+  val commandStream = ingestion.source.map(_.toString) // TODO: actually map to commands
+  val commandTopicStream = commandStream.map(streams.mapToCommandTopic)
+  commandTopicStream
+    .runWith(streams.commandSink)
     .foreach(_ => println("Done Producing"))
 
-  // kafka consumer
-  val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
-    .withBootstrapServers(kafkaServer)
-    .withGroupId("group1")
-    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+  // read from kafka
+  val commandStreamFromKafka = streams.sourceFromCommandTopic()
+  commandStreamFromKafka.mapAsync(1)(record => { // TODO: actually add the logic
+    println("out: " + record.value())
+    Future.successful(Done)
+  })
+  .runWith(Sink.ignore)
+  .foreach(_ => println("Done Consuming"))
 
-  val partition = 0
-  val offset = 0L // starts from beginnning; TODO: save offset somewhere
-  val subscription = Subscriptions.topics(config.kafka.topic)
-  val doneConsumer = Consumer.plainSource(consumerSettings, subscription)
-    .mapAsync(1)(record => {
-      println("out: " + record.value())
-      Future.successful(Done)
-    })
-    .runWith(Sink.ignore)
-    .foreach(_ => println("Done Consuming"))
 }
 
---
+++ bot/src/main/scala/science/wasabi/tini/bot/kafka/KafkaStreams.scala
@@ -0,0 +1,38 @@
+package science.wasabi.tini.bot.kafka
+
+import akka.kafka.scaladsl.{Consumer, Producer}
+import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer, StringDeserializer, StringSerializer}
+import science.wasabi.tini.bot.BotMain.system
+import science.wasabi.tini.config.Config.TiniConfig
+
+
+class KafkaStreams(implicit config: TiniConfig) {
+  val kafkaServer = s"${config.kafka.server}:${config.kafka.port}"
+
+  // kafka producer
+  private val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
+    .withBootstrapServers(kafkaServer)
+
+  val commandSink = Producer.plainSink(producerSettings)
+
+  def mapToCommandTopic(command: String): ProducerRecord[Array[Byte], String] = {
+    println("in: " + command)
+    new ProducerRecord[Array[Byte], String](config.kafka.topic, command)
+    }
+
+  def sourceFromCommandTopic() = {
+    // kafka consumer
+    val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+      .withBootstrapServers(kafkaServer)
+      .withGroupId("group1")
+      .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+    val partition = 0
+    val offset = 0L // starts from beginnning; TODO: save offset somewhere
+    val subscription = Subscriptions.topics(config.kafka.topic)
+    Consumer.plainSource(consumerSettings, subscription)
+  }
+}
